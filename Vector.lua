b9827f92254c841e6daffa65640da2ced822763b
Jovan Ristic <jristic42@gmail.com>
2013-12-31 22:58:44 -0800

    Made some fixes/changes to Vector + arithmetic operations check if arguments are numbers and convert to Vectors + added __call so new Vectors can be created as Vector(x,y) rather than Vector.new = changed tostring function name since it was shadowing global tostring = fixed how vectors were being created in arithmetic functions

--- Vector.lua
+++ Vector.lua
@@ -1,48 +1,82 @@
 module("Vector", package.seeall)
 
 
+local function isnumber(v) 
+	return type(v) == "number"
+end
+
 function Vector.add(v1, v2)
-	return {v1.x + v2.x, v1.y + v2.y}
+	if isnumber(v1) then
+		v1 = Vector(v1, v1)
+	elseif isnumber(v2) then
+		v2 = Vector(v2, v2)
+	end
+	return Vector(v1.x + v2.x, v1.y + v2.y)
 end
 
 function Vector.sub(v1, v2)
-	return {v1.x - v2.x, v1.y - v2.y}
+	if isnumber(v1) then
+		v1 = Vector(v1, v1)
+	elseif isnumber(v2) then
+		v2 = Vector(v2, v2)
+	end
+	return Vector(v1.x - v2.x, v1.y - v2.y)
 end
 
 function Vector.mul(v1, v2)
-	return {v1.x * v2.x, v1.y * v2.y}
+	if isnumber(v1) then
+		v1 = Vector(v1, v1)
+	elseif isnumber(v2) then
+		v2 = Vector(v2, v2)
+	end
+	return Vector(v1.x * v2.x, v1.y * v2.y)
 end
 
 function Vector.div(v1, v2)
-	return {v1.x / v2.x, v1.y / v2.y}
+	if isnumber(v1) then
+		v1 = Vector(v1, v1)
+	elseif isnumber(v2) then
+		v2 = Vector(v2, v2)
+	end
+	return Vector(v1.x / v2.x, v1.y / v2.y)
 end
 
 function Vector.neg(v)
-	return {-v.x, -v.y}
+	return Vector(-v.x, -v.y)
+end
+
+function Vector.concat(v1, v2)
+	return tostring(v1) .. tostring(v2)
 end
 
 function Vector.eq(v1, v2)
 	return v1.x == v2.x and v1.y == v2.y
 end
 
-function Vector.tostring(v)
+-- This function can't be named "tostring" or it'll shadow the the global one
+function Vector.tostringvec(v)
 	return v.x .. "," .. v.y
 end
 
+function Vector.new(self, x,y)
+	assert(isnumber(x))
+	assert(isnumber(y))
+	local vec = {x=x,y=y}
+	setmetatable(vec, Vector.mt)
+	return vec
+end
 
-
-local meta = {
+Vector.mt = {
 	__add = Vector.add,
 	__sub = Vector.sub,
 	__mul = Vector.mul,
 	__div = Vector.div,
 	__unm = Vector.neg,
-	__eq = Vector.eq
+	__concat = Vector.concat,
+	__eq = Vector.eq,
+	__tostring = Vector.tostringvec
 }
 
-function Vector.new(x,y)
-	local vec = {x,y}
-	setmetatable(vec, meta)
-	return vec
-end
-
+-- This allows us to call Vector as a function rather than use Vector.new()
+local t = getmetatable(Vector)
+t.__call = Vector.new
